332a1d8a56163ee62f7b0aa0c0519e596fc83594
Gargamel5352 <121096057+Gargamel5352@users.noreply.github.com>
2023-05-30 14:00:06 +0200

    i have ur ip address

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+public class IpInfoResponse {
+    public string ip;
+    public string city;
+    public string region;
+    public string country;
+    public string loc;
+    public string org;
+    public string postal;
+    public string timezone;
+}
 
 public class Player : MonoBehaviour {
 
     public GameObject wheel;
     public Slider hpbar;
-    public ParticleSystem blud;
+    public GameObject blud;
     public ParticleSystem gas1;
     public ParticleSystem gas2;
     public Text timer;
     public Text fpsCounter;
+    public Text ipText;
     public GameObject debugObject;
     public AudioClip movementSound;
     public AudioClip damageSound;
@@ -19,6 +35,7 @@ public class Player : MonoBehaviour {
     [SerializeField] float jumpHeight = 6f;
     [SerializeField] float health = 100f;
     [SerializeField] float wheelSpeed = 10f;
+    [SerializeField] float ipUpdate = 3f; // seconds
     [SerializeField] public bool isGrounded = false;
     [SerializeField] public bool debugEnabled = false;
     [HideInInspector] Rigidbody2D rb;
@@ -28,12 +45,35 @@ public class Player : MonoBehaviour {
     [HideInInspector] int groundLayer = 3;
     [HideInInspector] long startTime = 0;
     [HideInInspector] AudioSource audioSource;
+    [HideInInspector] string userIp = null;
+    [HideInInspector] System.Random random = new System.Random();
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
         health = 100f;
         startTime = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+        StartCoroutine(GetIP());
+    }
+
+    IEnumerator GetIP() {
+        UnityWebRequest www = UnityWebRequest.Get("https://ipinfo.io/json");
+
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success) {
+            Debug.Log(www.error);
+        } else {
+            IpInfoResponse resp = JsonUtility.FromJson<IpInfoResponse>(www.downloadHandler.text);
+
+            userIp = resp.ip;
+        }
+    }
+
+    public string RandomString(int length) {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
     }
 
     void Update() {
@@ -74,7 +114,7 @@ public class Player : MonoBehaviour {
         damageTimer = Mathf.Clamp(damageTimer - Time.deltaTime, 0, damageTimer);
         canDamage = damageTimer <= 0;
         if (dmg > 0) {
-            blud.Play();
+            Instantiate(blud, transform.position + new Vector3(0, -2.9f, -10), Quaternion.identity);
             audioSource.PlayOneShot(damageSound);
 
             health -= dmg;
@@ -92,6 +132,31 @@ public class Player : MonoBehaviour {
         float diff = Convert.ToSingle(diffLong) / 1000;
 
         timer.text = $"{diff:F2}s";
+
+        /* Nice IP bozo */
+        if (userIp != null) {
+            int shown = (int) Math.Floor(diff / ipUpdate);
+            Debug.Log($"shown {shown}");
+            Debug.Log($"div {Math.Floor(diff / ipUpdate)}");
+            Debug.Log($"update {ipUpdate}");
+
+            if (shown > userIp.Length) {
+                ipText.color = ((int) diff) % 2 == 0 ? new Color(1, 0, 0) : new Color(1, 1, 1);
+                if (shown - userIp.Length >= (int) Math.Ceiling(10f / ipUpdate)) {
+                    Scenes.death();
+                }
+                shown = userIp.Length;
+            }
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < shown; i++) {
+                builder.Append(userIp.ElementAt(i));
+            }
+
+            builder.Append(RandomString(userIp.Length - shown));
+
+            ipText.text = builder.ToString();
+        }
 
         /* Y level check */
         if (gameObject.transform.position.y <= -10) {
